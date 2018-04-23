37cc972a64017d10e786429d23b8c5e01d4b7636
Simonb the game designer <simonbut711@gmail.com>
2018-04-24 01:12:29 +0800

    ! enchance Distort_Test.cs structure

--- Unity/Assets/Resources/Distort_Test.cs
+++ Unity/Assets/Resources/Distort_Test.cs
@@ -10,24 +10,28 @@ public class Distort_Test : MonoBehaviour {
     // Use this for initialization
     internal IMAK_Distortion distortion_map1 = new IMAK_Distortion();
 
-	public GameObject Inter;
-	Vector3[] ver;
+    public float breathFreq = 1f;
+    public float blinkFreq = 1f;
+    public float breathMagnitude = 0.17f;
+    public string filename;
+
+    Vector3[] ver;
     void Start () {
         // = GameObject.Find("Distort1");
-        distortion_map1.Initialize(Inter);
-        distortion_map1.getMeshFromFile("HM_71", 10f);
+        distortion_map1.Initialize(this.gameObject);
+        distortion_map1.getMeshFromFile("HM_"+ filename, 10f);
         //distortion_map1.SetMaterial("Distort_Mat");
-        distortion_map1.SetTexture("71");
+        distortion_map1.SetTexture(filename);
 
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         time_clock = Time.time;
-		//distortion_map1.Distort_Anime.GetComponent<MeshFilter> ().mesh.vertices[5].x += 1f;
-		distortion_map1.Breath(time_clock, 0.1f, 1);
-		distortion_map1.blink (time_clock, time_clock%(5.0f),1.0f);
-		distortion_map1.Send_GPU_Command();
+        //distortion_map1.Distort_Anime.GetComponent<MeshFilter> ().mesh.vertices[5].x += 1f;
+        distortion_map1.Breath(time_clock, breathMagnitude, breathFreq);
+        distortion_map1.blink (time_clock, 5, blinkFreq);
+        distortion_map1.Send_GPU_Command();
 
     }
 
@@ -35,7 +39,7 @@ public class Distort_Test : MonoBehaviour {
 
     void OnGUI()
     {
-		
+        
     }
 }
 

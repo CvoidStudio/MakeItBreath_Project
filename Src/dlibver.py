e27ae8416ecaa910f802ad44c16b9dc310bc8dd4
Gabriel Cun <cvoidstudio@hotmail.com>
2018-04-25 10:40:21 +0800

    Updated: Compatible to Size

--- Src/dlibver.py
+++ Src/dlibver.py
@@ -8,7 +8,7 @@ from sys import exit
 EXE_Dir = argv[0]
 at = -1
 while EXE_Dir[at] != '\\':
-    at -= 1
+	at -= 1
 EXE_Dir = EXE_Dir[:at+1]
 
 argi = 0
@@ -16,43 +16,43 @@ src = ''
 out_dir = ''
 out_name = ''
 for item in argv:
-    if item == '-f':
-        src = argv[argi+1]
-    elif item == '-o':
-        out_dir = argv[argi+1]
-        if out_dir[-1] == '\\':
-            out_dir = out_dir[:-1]
-    elif item == '-n':
-        out_name = argv[argi+1]
-    argi += 1
-    
+	if item == '-f':
+		src = argv[argi+1]
+	elif item == '-o':
+		out_dir = argv[argi+1]
+		if out_dir[-1] == '\\':
+			out_dir = out_dir[:-1]
+	elif item == '-n':
+		out_name = argv[argi+1]
+	argi += 1
+	
 img1 = IMG.open(src)
-x_scale = im.size[0]/im.size[1]
+x_scale = img1.size[0]/img1.size[1]
 
 # Deal with 'P'
-if im.mode == 'P':
-    im = im.convert("RGBA")
+if img1.mode == 'P':
+	img1 = img1.convert("RGBA")
 
 src_name = (src.split('\\')[-1]).split('.')[0]
 
 def RGB2Gray(imgarr):
-    LenFormat = imgarr.shape[-1]
-    x = imgarr.astype('float')
-    Gray = np.floor((x[:,:,0]*0.299 + \
-            x[:,:,1]*0.587 + \
-            x[:,:,2]*0.114) \
-           * (x[:,:,3]/255.0 if LenFormat == 4
-           else 1.0)).astype('uint8')
-    return Gray
+	LenFormat = imgarr.shape[-1]
+	x = imgarr.astype('float')
+	Gray = np.floor((x[:,:,0]*0.299 + \
+			x[:,:,1]*0.587 + \
+			x[:,:,2]*0.114) \
+		   * (x[:,:,3]/255.0 if LenFormat == 4
+		   else 1.0)).astype('uint8')
+	return Gray
 	
 def SmoothStep(x,low,high):
-    if x < low:
-        return 0
-    elif x > high:
-        return 1
-    else:
-        return (x-low)/(high-low)
-    
+	if x < low:
+		return 0
+	elif x > high:
+		return 1
+	else:
+		return (x-low)/(high-low)
+	
 detector = get_frontal_face_detector()
 predictor = shape_predictor(EXE_Dir + 'shape_predictor_68_face_landmarks.dat')
 
@@ -63,34 +63,37 @@ im_size = tuple(gray1.shape)
 
 det1 = detector(gray1)
 
-
-from scipy.spatial import Delaunay
+from matplotlib.tri import Triangulation
 from copy import copy
 
 Points = []
 for index,face in enumerate(det1):
-    shape = predictor(gray1, face)
-    for index, pt in enumerate(shape.parts()):
-        Points.append([pt.x,pt.y])
-        
+	shape = predictor(gray1, face)
+	for index, pt in enumerate(shape.parts()):
+		Points.append([pt.x,pt.y])
+
+if len(Points) == 0:
+	print('Sorry, no character detected by current MIB >_<\n')
+	exit(0)
+		
 Eye_L_Low = Points[36:42]
 Eye_R_Low = Points[42:48]
 
 def Get_IntepPixel(Pts, FigArray):
-    x_l = np.floor(Pts[0])
-    x_h = x_l + 1
-    y_l = np.floor(Pts[1])
-    y_h = y_l + 1
-    p11 = FigArray[(int)(x_l)][(int)(y_l)]
-    p12 = FigArray[(int)(x_l)][(int)(y_h)]
-    p21 = FigArray[(int)(x_h)][(int)(y_l)]
-    p22 = FigArray[(int)(x_h)][(int)(y_h)]
-    return (int)((p11*(2-Pts[0]-Pts[1]+x_l+y_l) + \
-            p12*(2-Pts[0]+Pts[1]+x_l-y_h) + \
-            p21*(2+Pts[0]-Pts[1]-x_h+y_l) + \
-            p22*(2+Pts[0]+Pts[1]-x_h-y_h))/2)
-            
-            
+	x_l = np.floor(Pts[0])
+	x_h = x_l + 1
+	y_l = np.floor(Pts[1])
+	y_h = y_l + 1
+	p11 = FigArray[(int)(x_l)][(int)(y_l)]
+	p12 = FigArray[(int)(x_l)][(int)(y_h)]
+	p21 = FigArray[(int)(x_h)][(int)(y_l)]
+	p22 = FigArray[(int)(x_h)][(int)(y_h)]
+	return (int)((p11*(2-Pts[0]-Pts[1]+x_l+y_l) + \
+			p12*(2-Pts[0]+Pts[1]+x_l-y_h) + \
+			p21*(2+Pts[0]-Pts[1]-x_h+y_l) + \
+			p22*(2+Pts[0]+Pts[1]-x_h-y_h))/2)
+			
+			
 boarder1 = (gray1<50).astype('uint8')*255
 
 MaxStep = 4
@@ -99,8 +102,8 @@ Step = 1
 
 EyeCenter = np.array([0,0],dtype='float32')
 for p in Eye_L_Low:
-    EyeCenter[0] += p[0]
-    EyeCenter[1] += p[1]
+	EyeCenter[0] += p[0]
+	EyeCenter[1] += p[1]
 EyeCenter /= len(Eye_L_Low)
 
 # Corners
@@ -130,8 +133,8 @@ Step = 1
 
 EyeCenter = np.array([0,0],dtype='float32')
 for p in Eye_R_Low:
-    EyeCenter[0] += p[0]
-    EyeCenter[1] += p[1]
+	EyeCenter[0] += p[0]
+	EyeCenter[1] += p[1]
 EyeCenter /= len(Eye_R_Low)
 
 # Corners
@@ -159,60 +162,93 @@ Eye_R_High.append([6,DIR])
 Face_Out = list(copy(p) for p in Points[0:17])
 FaceCenter = np.array([0,0],dtype='float32')
 for p in Face_Out:
-    FaceCenter[0] += p[0]
-    FaceCenter[1] += p[1]
+	FaceCenter[0] += p[0]
+	FaceCenter[1] += p[1]
 FaceCenter /= len(Face_Out)
 
 out_size = 10
 for i in range(len(Face_Out)):
-    DIR = np.array([Face_Out[i][0]-FaceCenter[0],
-                    Face_Out[i][1]-FaceCenter[1]],dtype='float32')
-    DIR = DIR/np.linalg.norm(DIR)
-    Face_Out[i][0] += out_size*DIR[0]
-    Face_Out[i][1] += out_size*DIR[1]
-    
+	DIR = np.array([Face_Out[i][0]-FaceCenter[0],
+					Face_Out[i][1]-FaceCenter[1]],dtype='float32')
+	DIR = DIR/np.linalg.norm(DIR)
+	Face_Out[i][0] += out_size*DIR[0]
+	Face_Out[i][1] += out_size*DIR[1]
+	
 # 0: Others |
 # 1: head |
 # 2: eye_corner | 3: eye_upper | 4: eye_lower
-Final_PT = list([p[0],p[1],1,0,1] for p in Points)
-Final_PT[36][2] = 2
-Final_PT[39][2] = 2
-Final_PT[42][2] = 2
-Final_PT[45][2] = 2
+Final_PT = list([p[0],p[1],1,0,0] for p in Points)
 
-U_Alpha = 0.7
-D_Alpha = 1.0 - U_Alpha
+U_Alpha = 7.0
+D_Alpha = 10.0 - U_Alpha
+
 Final_PT[37][2] = 3
 Final_PT[37][3] = (Final_PT[41][0]-Final_PT[37][0])*U_Alpha
+Final_PT[37][3] /= (float)(im_size[0])
 Final_PT[37][4] = (Final_PT[41][1]-Final_PT[37][1])*U_Alpha
+Final_PT[37][4] /= (float)(im_size[0])
+
+Final_PT[36][2] = 2
+Final_PT[36][3] = Final_PT[37][3]*0.2
+Final_PT[36][4] = Final_PT[37][4]*0.2
+
+
+
 
 Final_PT[38][2] = 3
 Final_PT[38][3] = (Final_PT[40][0]-Final_PT[38][0])*U_Alpha
+Final_PT[38][3] /= (float)(im_size[0])
 Final_PT[38][4] = (Final_PT[40][1]-Final_PT[38][1])*U_Alpha
-
+Final_PT[38][4] /= (float)(im_size[0])
 Final_PT[40][2] = 4
 Final_PT[40][3] = (Final_PT[38][0]-Final_PT[40][0])*D_Alpha
+Final_PT[40][3] /= (float)(im_size[0])
 Final_PT[40][4] = (Final_PT[38][1]-Final_PT[40][1])*D_Alpha
+Final_PT[40][4] /= (float)(im_size[0])
+
+Final_PT[39][2] = 2
+Final_PT[39][3] = Final_PT[38][3]*0.2
+Final_PT[39][4] = Final_PT[38][4]*0.2
 
 Final_PT[41][2] = 4
 Final_PT[41][3] = (Final_PT[37][0]-Final_PT[41][0])*D_Alpha
+Final_PT[41][3] /= (float)(im_size[0])
 Final_PT[41][4] = (Final_PT[37][1]-Final_PT[41][1])*D_Alpha
+Final_PT[41][4] /= (float)(im_size[0])
 
 Final_PT[43][2] = 3
 Final_PT[43][3] = (Final_PT[47][0]-Final_PT[43][0])*U_Alpha
+Final_PT[43][3] /= (float)(im_size[0])
 Final_PT[43][4] = (Final_PT[47][1]-Final_PT[43][1])*U_Alpha
+Final_PT[43][4] /= (float)(im_size[0])
 
 Final_PT[44][2] = 3
 Final_PT[44][3] = (Final_PT[46][0]-Final_PT[44][0])*U_Alpha
+Final_PT[44][3] /= (float)(im_size[0])
 Final_PT[44][4] = (Final_PT[46][1]-Final_PT[44][1])*U_Alpha
+Final_PT[44][4] /= (float)(im_size[0])
+
+Final_PT[42][2] = 2
+Final_PT[42][3] = Final_PT[43][3]*0.2
+Final_PT[42][4] = Final_PT[43][4]*0.2
+
+Final_PT[45][2] = 2
+Final_PT[45][3] = Final_PT[44][3]*0.2
+Final_PT[45][4] = Final_PT[44][4]*0.2
 
 Final_PT[46][2] = 4
 Final_PT[46][3] = (Final_PT[44][0]-Final_PT[46][0])*D_Alpha
+Final_PT[46][3] /= (float)(im_size[0])
 Final_PT[46][4] = (Final_PT[44][1]-Final_PT[46][1])*D_Alpha
+Final_PT[46][4] /= (float)(im_size[0])
 
 Final_PT[47][2] = 4
 Final_PT[47][3] = (Final_PT[43][0]-Final_PT[47][0])*D_Alpha
+Final_PT[47][3] /= (float)(im_size[0])
 Final_PT[47][4] = (Final_PT[43][1]-Final_PT[47][1])*D_Alpha
+Final_PT[47][4] /= (float)(im_size[0])
+
+
 
 # upper boarder
 Face_Center = [0.0,0.0]
@@ -220,84 +256,97 @@ Face_Out_num = 0
 FO_Start = len(Final_PT)
 FACES = []
 for index,face in enumerate(det1):
-    FACES.append(face)
-    for x in np.linspace(face.left(),face.right(),4):
-        Final_PT.append([x,face.top(),1,0,1])
-        Face_Center[0] += x
-        Face_Center[1] += face.top()
-        Face_Out_num += 1
+	FACES.append(face)
+	for x in np.linspace(face.left(),face.right(),4):
+		Final_PT.append([x,face.top(),1,0,1])
+		Face_Center[0] += x
+		Face_Center[1] += face.top()
+		Face_Out_num += 1
 
 for p in Face_Out:
-    Final_PT.append([p[0],p[1],1,0,1])
-    Face_Center[0] += p[0]
-    Face_Center[1] += p[1]
-    Face_Out_num += 1
-    #F_Out.append((p[0],p[1]))
+	Final_PT.append([p[0],p[1],1,0,1])
+	Face_Center[0] += p[0]
+	Face_Center[1] += p[1]
+	Face_Out_num += 1
+	#F_Out.append((p[0],p[1]))
 
 Face_Center[0] /= (float)(Face_Out_num)
 Face_Center[1] /= (float)(Face_Out_num)
 F_Radius = 0.0
 for op in Final_PT[FO_Start:]:
-    R_Temp = (op[0]-Face_Center[0])**2 + (op[1]-Face_Center[1])**2
-    if R_Temp > F_Radius:
-        F_Radius = R_Temp
+	R_Temp = (op[0]-Face_Center[0])**2 + (op[1]-Face_Center[1])**2
+	if R_Temp > F_Radius:
+		F_Radius = R_Temp
 # eye boarder
 #Final_PT.append([p[0],p[1],1])
 
 # uniform points
 for x in range(0,im_size[1],20):
-    if im_size[1] - x < 20:
-        x = im_size[1]
-    x_dist = x-Face_Center[0]
-    x_shift = SmoothStep(x_dist,0,im_size[1]/2)
-    for y in range(0,FACES[0].top()-20,20):
-        _Dist =  x_dist**2 + (y-Face_Center[1])**2
-        Final_PT.append([x,y,0,x_shift,0])
-    for y in range(im_size[0],FACES[0].bottom()+20,-20):
-        _Dist = x_dist**2 + (y-Face_Center[1])**2
-        y_ss = 0.0
-        if im_size[0] - y < 64:
-            y_ss = 1.0 - SmoothStep(im_size[0] - y,0,64)
-        else:
-            y_ss = SmoothStep(_Dist,0,400)
-            
-        Final_PT.append([x,y,0,x_shift,y_ss])
+	if im_size[1] - x < 20:
+		x = im_size[1]
+	x_dist = x-Face_Center[0]
+	x_shift = SmoothStep(x_dist,0,im_size[1]/2)
+	# boarder
+	if x < 64:
+		x_shift *= SmoothStep(x,0,64)
+	elif im_size[1]-x < 64:
+		x_shift *= SmoothStep(im_size[1]-x,0,64)
+		
+	for y in range(0,FACES[0].top()-20,20):
+		_Dist =  x_dist**2 + (y-Face_Center[1])**2
+		y_shift = SmoothStep(_Dist,0,400)
+		if y < 64:
+			y_shift *= SmoothStep(y,0,64)
+		Final_PT.append([x,y,0,x_shift,y_shift])
+	for y in range(im_size[0],FACES[0].bottom()+20,-20):
+		_Dist = x_dist**2 + (y-Face_Center[1])**2
+		y_ss = SmoothStep(_Dist,0,400)
+		if im_size[0] - y < 64:
+			y_ss *= 1.0 - SmoothStep(im_size[0] - y,0,64)
+ 
+		Final_PT.append([x,y,0,x_shift,y_ss])
 
 for y in range(FACES[0].top(),FACES[0].bottom(),20):
-    y_dist = (y-Face_Center[1])**2
-    for x in range(0,FACES[0].left()-20,20):
-        x_dist = (x-Face_Center[0])**2
-        x_shift = SmoothStep(x_dist,F_Radius,(im_size[1]**2)/4)
-        _Dist = x_dist + y_dist
-        Final_PT.append([x,y,0,-x_shift,SmoothStep(_Dist,0,400)])
-    for x in range(im_size[1],FACES[0].right()+20,-20):
-        x_dist = (x-Face_Center[0])**2
-        x_shift = SmoothStep(x_dist,F_Radius,(im_size[1]**2)/4)
-        _Dist = x_dist + y_dist
-        Final_PT.append([x,y,0,x_shift,SmoothStep(_Dist,0,400)])
-        
+	y_dist = (y-Face_Center[1])**2
+	for x in range(0,FACES[0].left()-20,20):
+		x_dist = (x-Face_Center[0])**2
+		x_shift = SmoothStep(x_dist,F_Radius,(im_size[1]**2)/4)
+		if x < 64:
+			x_shift *= SmoothStep(x,0,64)
+		_Dist = x_dist + y_dist
+		Final_PT.append([x,y,0,-x_shift,SmoothStep(_Dist,0,400)])
+	for x in range(im_size[1],FACES[0].right()+20,-20):
+		x_dist = (x-Face_Center[0])**2
+		x_shift = SmoothStep(x_dist,F_Radius,(im_size[1]**2)/4)
+		if im_size[1]-x < 64:
+			x_shift *= SmoothStep(im_size[1]-x,0,64)
+		_Dist = x_dist + y_dist
+		Final_PT.append([x,y,0,x_shift,SmoothStep(_Dist,0,400)])
+		
 
-        
+		
 # Triangulation
-delaunay_Tri = Delaunay(list([p[0],p[1]] for p in Final_PT)).simplices.copy()
+delaunay_Tri = Triangulation(list(p[0] for p in Final_PT),list(p[1] for p in Final_PT)).triangles
 
 
 # print out heatmap
 if not out_name:
-    out_name = src_name
+	out_name = src_name
 
 FW = open(out_dir + '\\HM_%s.json'%(out_name),'w')
 FW.write('#,%i,%i\n'%(len(Final_PT),len(delaunay_Tri)))
 
 for p in Final_PT:
-    line = '%.3f,%.3f,%.3f,%.3f,%i,%.3f,%.3f\n'%\
-    ((float)(p[0])/(float)(im_size[1])*((float)(im_size[1])/im_size[0]),(float)(p[1])/(float)(im_size[0]),
-     (float)(p[0])/(float)(im_size[1]),1.0 - (float)(p[1])/(float)(im_size[0]),
-     p[2],
-     p[3],p[4])
-    FW.write(line)
+	line = '%.3f,%.3f,%.3f,%.3f,%i,%.3f,%.3f\n'%\
+	((float)(p[0])/(float)(im_size[1])*((float)(im_size[1])/im_size[0]),(float)(p[1])/(float)(im_size[0]),
+	 (float)(p[0])/(float)(im_size[1]),1.0 - (float)(p[1])/(float)(im_size[0]),
+	 p[2],
+	 p[3],p[4])
+	FW.write(line)
 
 for Tri in delaunay_Tri:
-    line = '%i,%i,%i\n'%(Tri[0],Tri[1],Tri[2])
-    FW.write(line)
+	line = '%i,%i,%i\n'%(Tri[0],Tri[1],Tri[2])
+	FW.write(line)
 FW.close()
+
+print('finished.')
